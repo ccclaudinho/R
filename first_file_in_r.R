5f1dc2047af9f43cf1bd16774e6e2a1adf5c97ce
Cláudio Roberto Celestino Júnior <112024522+ccclaudinho@users.noreply.github.com>
2023-06-26 15:53:01 -0300

    hello world in R

---
+++ first_file_in_r.R
@@ -0,0 +1,24 @@
+família <- c("Maria", "Cláudio", "Juninho", "Ligia")
+print(família[1])
+print(família[4])
+idades <- c(48,51,33,31)
+print (idades[1])
+idades
+família
+print(família)
+print(família)
+print (idades)
+teste <- c(família,idades)
+print(teste)
+mean(idades)
+sd(idades)
+hist(idades)
+k <- 0
+x <- c(1,2,3,4,5,6,7,8,9,10)
+for (n in x){
+  print (n)
+  if(n %% 2==0){
+    k<-k+1
+  }
+}
+print(k)
